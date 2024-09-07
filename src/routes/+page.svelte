43c9de04966a21d05742795ab5bc6a8fcc4e3782
KimJammer <41841812+kimjammer@users.noreply.github.com>
2024-09-06 23:12:47 -0400

    Google Login Test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import {onMount} from "svelte";
+
+    function handleCredentialResponse(response) {
+        console.log("Encoded JWT ID token: " + response.credential);
+    }
+
+    onMount(() => {
+        google.accounts.id.initialize({
+            client_id: "358229656846-r8l2ot51j16mt7hdd6g63o9g3p5qo4p1.apps.googleusercontent.com",
+            callback: handleCredentialResponse
+        });
+        google.accounts.id.renderButton(
+            document.getElementById("buttonDiv"),
+            { theme: "outline", size: "large", text: "continue_with" }  // customization attributes
+        );
+        //google.accounts.id.prompt(); // also display the One Tap dialog
+    });
+</script>
+
+<svelte:head>
+    <script src="https://accounts.google.com/gsi/client" async></script>
+</svelte:head>
+
+<h1>Welcome to Nebula Auth test</h1>
+<div id="buttonDiv"></div>
