56dad4f50e5c87970f5b87061c4e820ca16f4983
danielkzhang11 <danielkzhang11@gmail.com>
2024-09-27 21:38:19 -0400

    implemented getFeed

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -2,36 +2,46 @@
     import {onMount} from "svelte";
     import '../../app.css';
 
-    let posts = [
-        {
-            "post_id": "abcde",
-            "content": "This is a post",
-            "timestamp": "1727482798",
-            "posterId": "12345",
-            "username": "user1"
-        },
-        {
-            "post_id": "abcde",
-            "content": "This is a post2",
-            "timestamp": "1727482798",
-            "posterId": "12345",
-            "username": "user1"
-        },
-        {
-            "post_id": "abcde",
-            "content": "This is a post3",
-            "timestamp": "1727482798",
-            "posterId": "12345",
-            "username": "user1"
+    let posts:any = [];
+
+    async function getFeed() {
+        try {
+            let rawResponse = await fetch("http://localhost:5000/user/feed", {
+                method: "GET",
+                credentials: "include",
+                headers: {
+                    'Accept': 'application/json',
+                }
+            })
+            let response = await rawResponse.json();
+            posts = response.posts;
+        } catch (error) {
+            // Not logged in
         }
-    ];
+    }
 
     onMount(() => {
         //Call API
     });
+    
 </script>
 
-<div class="flex justify-center">
-
+<div>
+    {#each posts as post}
+        <div class="flex justify-center w-75">
+            <div class="card bg-base-100 w-100 shadow-xl">
+                <div class="card-body items-start text-center">
+                    <h2 class="card-title">{post.username}</h2>
+                    <p>{post.content}</p>
+                    <figure>
+                        <img class = "max-w-[450px]" 
+                        src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                        alt="Shoes" />
+                    </figure>
+                </div>
+            </div>
+        </div>
+    {/each}
 </div>
+
 
