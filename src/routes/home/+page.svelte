d4674d6121f903e7a4306be2026251c8270a623f
KimJammer <41841812+kimjammer@users.noreply.github.com>
2024-09-27 20:21:01 -0400

    Create dummy feed page

---
+++ src/routes/home/+page.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import {onMount} from "svelte";
+    import '../../app.css';
+
+    let posts = [
+        {
+            "post_id": "abcde",
+            "content": "This is a post",
+            "timestamp": "1727482798",
+            "posterId": "12345",
+            "username": "user1"
+        },
+        {
+            "post_id": "abcde",
+            "content": "This is a post2",
+            "timestamp": "1727482798",
+            "posterId": "12345",
+            "username": "user1"
+        },
+        {
+            "post_id": "abcde",
+            "content": "This is a post3",
+            "timestamp": "1727482798",
+            "posterId": "12345",
+            "username": "user1"
+        }
+    ];
+
+    onMount(() => {
+        //Call API
+    });
+</script>
+
+<div class="flex justify-center">
+
+</div>
+
